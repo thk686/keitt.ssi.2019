43e9098e185d29f3202dc301e6741bfaa5ab96b1
Tim Keitt <tkeitt@gmail.com>
2019-05-28 11:38:12 -0500

    updated site

--- R/utils.R
+++ R/utils.R
@@ -10,19 +10,19 @@ get_materials_path <- function() system.file("materials", package = "keitt.ssi.2
     packageStartupMessage("Run: vignette('Introduction', package='keitt.ssi.2019')")
 }
 
-#' @export
-fortify.RasterLayer <- function(x, maxpixels = 50000){
-  raster <- raster::sampleRegular(x, maxpixels, asRaster = TRUE)
-  if(nlayers(x) == 1 && is.factor(x)) raster <- raster::stack(raster,raster)  ## workaround raster bug #6043
-  raster::as.data.frame(raster, xy = TRUE)
-}
-
-#' @export
-fortify.RasterBrick <- function(...){
-  fortify.RasterLayer(...)
-}
-
-#' @export
-fortify.RasterStack <- function(...){
-  fortify.RasterLayer(...)
-}
+#' #' @export
+#' fortify.RasterLayer <- function(x, maxpixels = 50000){
+#'   raster <- raster::sampleRegular(x, maxpixels, asRaster = TRUE)
+#'   if(nlayers(x) == 1 && is.factor(x)) raster <- raster::stack(raster,raster)  ## workaround raster bug #6043
+#'   raster::as.data.frame(raster, xy = TRUE)
+#' }
+#'
+#' #' @export
+#' fortify.RasterBrick <- function(...){
+#'   fortify.RasterLayer(...)
+#' }
+#'
+#' #' @export
+#' fortify.RasterStack <- function(...){
+#'   fortify.RasterLayer(...)
+#' }
