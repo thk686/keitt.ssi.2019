72b94c5782242930ee8d567e4ede61c4777b7d76
Tim Keitt <tkeitt@gmail.com>
2019-05-28 11:33:36 -0500

    added mapping vignette

--- R/utils.R
+++ R/utils.R
@@ -6,5 +6,23 @@
 get_materials_path <- function() system.file("materials", package = "keitt.ssi.2019")
 
 .onAttach <- function (libname, pkgname) {
-  packageStartupMessage("Run: vignette('Introduction', package='keitt.ssi.2019')")
+  if(interactive())
+    packageStartupMessage("Run: vignette('Introduction', package='keitt.ssi.2019')")
+}
+
+#' @export
+fortify.RasterLayer <- function(x, maxpixels = 50000){
+  raster <- raster::sampleRegular(x, maxpixels, asRaster = TRUE)
+  if(nlayers(x) == 1 && is.factor(x)) raster <- raster::stack(raster,raster)  ## workaround raster bug #6043
+  raster::as.data.frame(raster, xy = TRUE)
+}
+
+#' @export
+fortify.RasterBrick <- function(...){
+  fortify.RasterLayer(...)
+}
+
+#' @export
+fortify.RasterStack <- function(...){
+  fortify.RasterLayer(...)
 }
